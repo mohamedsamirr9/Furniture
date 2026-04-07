450b063fc129b689ac5f88c9e96e691a94e1efc0
SoadWalid <soadwalid964@gmail.com>
2026-04-08 00:54:24 +0200

    Add Wishlist UI with View and Cart buttons

---
+++ frontend/src/app/features/public/wishlist/wishlist/wishlist.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-wishlist',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './wishlist.html',
+  styleUrl: './wishlist.css',
+
+
+})
+export class WishlistComponent implements OnInit {
+
+  loading = true;
+
+  wishlistItems = [
+    {
+      id: 1,
+      name: 'Modern Chair',
+      price: 500,
+      originalPrice: 650,
+      rating: 4,
+      reviewCount: 12,
+      mainImage: 'assets/image1.jpg',
+      brand: 'IKEA'
+    },
+    {
+      id: 2,
+      name: 'Wooden Table',
+      price: 1200,
+      rating: 5,
+      reviewCount: 30,
+      mainImage: 'assets/image2.jpg',
+      brand: 'Home Center'
+    },
+    {
+      id: 3,
+      name: 'Luxury Sofa',
+      price: 3000,
+      rating: 5,
+      reviewCount: 55,
+      mainImage: 'assets/image3.jpg',
+      brand: 'Furnora'
+    }
+  ];
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+  }
+viewProduct(id: number) {
+  // هنا ممكن تستخدم Router عشان تروح لصفحة تفاصيل المنتج
+  // مثال:
+  // this.router.navigate(['/products', id]);
+  alert('Go to product details for ID: ' + id);
+}
+  removeItem(id: number) {
+    this.wishlistItems = this.wishlistItems.filter(item => item.id !== id);
+  }
+
+  addToCart(product: any) {
+    alert(product.name + ' added to cart 🛒');
+  }
+
+  getStars(): number[] {
+    return [1, 2, 3, 4, 5];
+  }
+}
