1992046b6bdaf32fcc7478bb1b2cc830d2c1e9a4
Mustafa Abo El-Magd <msaboelmagd1998@gmail.com>
2026-04-08 02:16:56 +0200

    complaints dto, complaints mapper, complaints specifications, services

---
+++ backend/Furniture.Services/Mapping/MappingComplaint.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Furniture.Domain.Models;
+using Furniture.shared.Dtos.ComplaintsDto;
+using System;   
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furniture.Services.Mapping
+{
+    public class MappingComplaint: Profile
+    {
+        public MappingComplaint()
+        {
+            //when you create a list
+            CreateMap<Complaint, ComplaintDto>()
+                .ForMember(d => d.Status, o => o.MapFrom(s => s.Status.ToString()));
+            //details 
+            CreateMap<Complaint, ComplaintDetails>()    
+
+
+        }
+    }
+}
---
+++ backend/Furniture.Servises Abstraction/IComplaintService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furniture.Servises_Abstraction
+{
+    internal interface IComplaintService
+    {
+    }
+}
